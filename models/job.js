0b7cce3450da04a213e8efa53f8f03e61d7947f2
Jennie Mauch <jenniemauch@me.com>
2022-07-13 21:51:10 -0400

    routes

---
+++ models/job.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const Job = mongoose.model("Job", jobSchema);
+module.exports = Job;
---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+    {
+      email: {
+        type: String,
+        required: true,
+      },
+      password: {
+        type: String,
+        required: true,
+      },
+      first_name: {
+        type: String,
+        required: true,
+      }, 
+      last_name: {
+        type: String,
+        required: true,
+      }
+    },
+  );
+
+const User = mongoose.model("user", userSchema);
+module.exports = User;
